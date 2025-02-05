ba05f29b78865da89e2f4451233467146b7d520b
Marwan <marwan-walid1@hotmail.com>
2025-02-05 08:43:52 +0200

    Initial commit

---
+++ G04 - OOP Exam/Exams.cs
@@ -0,0 +1,6 @@
+namespace G04___OOP_Exam;
+
+public class Exams
+{
+    
+}
---
+++ G04 - OOP Exam/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace G04___OOP_Exam;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
